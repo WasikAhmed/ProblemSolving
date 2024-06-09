d51e922c5129129f10641b9dfc3b077d05888590
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-06-10 00:18:44 +0600

    Update leetcode prob 66(Plus One)

--- LeetCode/StudyPlan/ProgrammingSkills/66.py
+++ LeetCode/StudyPlan/ProgrammingSkills/66.py
@@ -4,19 +4,29 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        integer = 0
-        for digit in digits:
-            integer = (integer * 10) + digit
-        integer += 1
+        # integer = 0
+        # for digit in digits:
+        #     integer = (integer * 10) + digit
+        # integer += 1
+        #
+        # ans = []
+        # while integer > 0:
+        #     ans.insert(0, integer % 10)
+        #     integer = integer // 10
+        #
+        # return ans
 
-        ans = []
-        while integer > 0:
-            ans.insert(0, integer % 10)
-            integer = integer // 10
+        for i in reversed(range(len(digits))):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
 
-        return ans
+        return [1] + digits
 
 
 s = Solution()
 print(s.plusOne([1, 2, 3]))
 print(s.plusOne([4, 3, 2, 1]))
+print(s.plusOne([9]))
