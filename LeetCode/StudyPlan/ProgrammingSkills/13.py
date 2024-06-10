c46413feeb4344a49f79c9c272166b1d74caac38
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-06-10 23:08:12 +0600

    Update leetcode prob 13(Roman to Integer)

--- LeetCode/StudyPlan/ProgrammingSkills/13.py
+++ LeetCode/StudyPlan/ProgrammingSkills/13.py
@@ -8,18 +8,27 @@ class Solution:
         value_table = dict(zip(symbols, values))
 
         integer = 0
+        # for i in range(len(s)):
+        #     if i < len(s) - 1 and s[i] in ['I', 'X', 'C']:
+        #         if s[i] == 'I' and (s[i + 1] == 'V' or s[i + 1] == 'X'):
+        #             integer -= value_table.get(s[i])
+        #         elif s[i] == 'X' and (s[i + 1] == 'L' or s[i + 1] == 'C'):
+        #             integer -= value_table.get(s[i])
+        #         elif s[i] == 'C' and (s[i + 1] == 'D' or s[i + 1] == 'M'):
+        #             integer -= value_table.get(s[i])
+        #         else:
+        #             integer += value_table.get(s[i])
+        #     else:
+        #         integer += value_table.get(s[i])
+        # return integer
+
+        integer = 0
         for i in range(len(s)):
-            if i < len(s) - 1 and s[i] in ['I', 'X', 'C']:
-                if s[i] == 'I' and (s[i + 1] == 'V' or s[i + 1] == 'X'):
-                    integer -= value_table.get(s[i])
-                elif s[i] == 'X' and (s[i + 1] == 'L' or s[i + 1] == 'C'):
-                    integer -= value_table.get(s[i])
-                elif s[i] == 'C' and (s[i + 1] == 'D' or s[i + 1] == 'M'):
-                    integer -= value_table.get(s[i])
-                else:
-                    integer += value_table.get(s[i])
+            if i < len(s) - 1 and value_table[s[i]] < value_table[s[i+1]]:
+                integer -= value_table[s[i]]
             else:
-                integer += value_table.get(s[i])
+                integer += value_table[s[i]]
+
         return integer
 
 
