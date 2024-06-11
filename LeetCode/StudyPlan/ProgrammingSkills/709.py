b06eeb93b25e3d93c93d1eb6f597eefececd3898
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-06-11 22:52:02 +0600

    Solved leetcode prob 709(To Lower Case)

---
+++ LeetCode/StudyPlan/ProgrammingSkills/709.py
@@ -0,0 +1,11 @@
+# To Lower Case
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+
+
+s = Solution()
+print(s.toLowerCase("Hello"))
+print(s.toLowerCase("here"))
+print(s.toLowerCase("LOVELY"))
