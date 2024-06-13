99749caa405f7f7649fcfd15f48fdce2b6236cd4
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-06-13 19:16:11 +0600

    Update LeetCode prob 389(Find the Difference) solution.
    Signed-off-by: Wasik Ahmed Apon <apon.wasik2001@gmail.com>

--- LeetCode/StudyPlan/ProgrammingSkills/389.py
+++ LeetCode/StudyPlan/ProgrammingSkills/389.py
@@ -2,13 +2,17 @@
 
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s = ''.join(sorted(s))
-        t = ''.join(sorted(t))
+        # s = ''.join(sorted(s))
+        # t = ''.join(sorted(t))
+        #
+        # for i in range(len(s)):
+        #     if s[i] != t[i]:
+        #         return t[i]
+        # return t[-1]
 
-        for i in range(len(s)):
-            if s[i] != t[i]:
-                return t[i]
-        return t[-1]
+        for char in t:
+            if s.count(char) != t.count(char):
+                return char
 
 
 s = Solution()
