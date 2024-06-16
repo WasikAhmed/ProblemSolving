8ffd1d9d72c6cb5ab51f45b8db267d81d9bc0eab
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-06-17 00:20:53 +0600

    Update solution for Codeforces prob 381A (Sereja and Dima)

--- CodeForces/381A.py
+++ CodeForces/381A.py
@@ -8,20 +8,37 @@ cards = list(map(int, sys.stdin.readline().strip().split()))
 l, r = 0, n - 1
 sereja, dima = 0, 0
 
-for i in range(n):
-    if i % 2 == 0:
-        if cards[r] >= cards[l]:
+seraja_turn = True
+while l <= r:
+    if cards[r] > cards[l]:
+        if seraja_turn:
             sereja += cards[r]
-            r -= 1
         else:
-            sereja -= cards[l]
-            l += 1
-    else:
-        if cards[r] >= cards[l]:
             dima += cards[r]
-            r -= 1
+        r -= 1
+    else:
+        if seraja_turn:
+            sereja += cards[l]
         else:
             dima += cards[l]
-            l += 1
+        l += 1
+
+    seraja_turn = not seraja_turn
+
+# for i in range(n):
+#     if i % 2 == 0:
+#         if cards[r] >= cards[l]:
+#             sereja += cards[r]
+#             r -= 1
+#         else:
+#             sereja += cards[l]
+#             l += 1
+#     else:
+#         if cards[r] >= cards[l]:
+#             dima += cards[r]
+#             r -= 1
+#         else:
+#             dima += cards[l]
+#             l += 1
 
 sys.stdout.write(f'{sereja} {dima}')
