a2c62d1d1841851f5c5e4eaebe55b712fcc6e245
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-06-25 20:34:28 +0600

    Update solution for LeetCode prob 14 (Longest Common Prefix)

--- LeetCode/14.py
+++ LeetCode/14.py
@@ -5,14 +5,21 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        strs = sorted(strs)
-        ans = ''
-        for i in range(len(strs[0])):
-            if strs[0][i] == strs[-1][i]:
-                ans += strs[0][i]
-            else:
-                break
-        return ans
+        # strs = sorted(strs)
+        # ans = ''
+        # for i in range(len(strs[0])):
+        #     if strs[0][i] == strs[-1][i]:
+        #         ans += strs[0][i]
+        #     else:
+        #         break
+        # return ans
+
+        mini, maxi = min(strs), max(strs)
+
+        for i in range(len(mini)):
+            if mini[i] != maxi[i]:
+                return mini[:i]
+        return mini
 
 
 if __name__ == '__main__':
