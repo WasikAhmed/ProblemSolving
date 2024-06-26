a63884b3cf77e7c54e4a813f197b38e5f54f3673
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-06-27 00:26:54 +0600

    Optimize solution for Codeforces prob 1409A (Yet Another Two Integers Problem)

--- CodeForces/1409A.py
+++ CodeForces/1409A.py
@@ -4,19 +4,27 @@ import sys
 
 input = sys.stdin.read().strip().split('\n')
 
-integer = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+# integer = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
 t = int(input[0].strip())
 index = 1
 
 for _ in range(t):
-    arr = list(map(int, input[index].strip().split()))
+    # arr = list(map(int, input[index].strip().split()))
+    # index += 1
+    #
+    # diff = abs(arr[0] - arr[1])
+    # ans = 0
+    # for i in integer:
+    #     if diff >= i:
+    #         ans += (diff // i)
+    #         diff -= (diff // i) * i
+    #
+    # sys.stdout.write(f'{ans}\n')
+
+    a, b = map(int, input[index].strip().split())
     index += 1
 
-    diff = abs(arr[0] - arr[1])
-    ans = 0
-    for i in integer:
-        if diff >= i:
-            ans += (diff // i)
-            diff -= (diff // i) * i
-
+    diff = abs(a - b)
+    ans = (diff + 9) // 10
     sys.stdout.write(f'{ans}\n')
