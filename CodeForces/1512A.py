caf48098821d1b9206fe232b988a3905291141bd
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-06-26 17:36:58 +0600

    Update solution for Codeforces prob 1512A (Spy Detected!)

--- CodeForces/1512A.py
+++ CodeForces/1512A.py
@@ -1,6 +1,7 @@
 # Spy Detected!
 
 import sys
+from collections import Counter
 
 input = sys.stdin.read().strip().split('\n')
 
@@ -13,10 +14,15 @@ for _ in range(t):
     arr = list(map(int, input[index].strip().split()))
     index += 1
 
-    s = set(arr)
-    a, b = iter(s)
+    # s = set(arr)
+    # a, b = iter(s)
+    #
+    # if arr.count(a) < arr.count(b):
+    #     sys.stdout.write(f'{arr.index(a) + 1}\n')
+    # else:
+    #     sys.stdout.write(f'{arr.index(b) + 1}\n')
 
-    if arr.count(a) < arr.count(b):
-        sys.stdout.write(f'{arr.index(a) + 1}\n')
-    else:
-        sys.stdout.write(f'{arr.index(b) + 1}\n')
+    counts = Counter(arr)
+    unique = [num for num, count in counts.items() if count == 1][0]
+
+    sys.stdout.write(f'{arr.index(unique) + 1}\n')
