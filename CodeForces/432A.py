672e7974555ddab880300465f0b0db0b121bb3dd
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-06-27 16:25:57 +0600

    Solved Codeforces prob 432A (Choosing Teams)

---
+++ CodeForces/432A.py
@@ -0,0 +1,15 @@
+# Choosing Teams
+
+import sys
+
+input = sys.stdin.read().strip().split('\n')
+
+t, k = map(int, input[0].strip().split())
+participation = list(map(int, input[1].strip().split()))
+
+eligible = 0
+for x in participation:
+    if 5 - x >= k:
+        eligible += 1
+
+sys.stdout.write(f'{eligible // 3}\n')
