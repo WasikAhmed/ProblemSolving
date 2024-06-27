a50b69c6ca1b3ad1c7dc19930e5e766fc96041e8
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-06-27 16:33:12 +0600

    Add list comprehension for the solution of Codeforces prob 432A

--- CodeForces/432A.py
+++ CodeForces/432A.py
@@ -7,9 +7,11 @@ input = sys.stdin.read().strip().split('\n')
 t, k = map(int, input[0].strip().split())
 participation = list(map(int, input[1].strip().split()))
 
-eligible = 0
-for x in participation:
-    if 5 - x >= k:
-        eligible += 1
+# eligible = 0
+# for x in participation:
+#     if 5 - x >= k:
+#         eligible += 1
+
+eligible = len([x for x in participation if 5 - x >= k])
 
 sys.stdout.write(f'{eligible // 3}\n')
