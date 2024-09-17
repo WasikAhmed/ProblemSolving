7fca98564408510672817431e3a8ac14971a41f4
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-09-18 00:01:00 +0600

    Add a solution for LeetCode prob 1(Two Sum)

--- LeetCode/1.cpp
+++ LeetCode/1.cpp
@@ -2,19 +2,37 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         for(int i=0; i<nums.size()-1; i++) {
+//             for(int j=i+1; j<nums.size(); j++) {
+//                 if(nums[i] + nums[j] == target) {
+//                     return vector<int>{i, j};
+//                 }
+//             }
+//         }
+//         return vector<int>{};
+//     }
+// };
+
+// using hashmap
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size()-1; i++) {
-            for(int j=i+1; j<nums.size(); j++) {
-                if(nums[i] + nums[j] == target) {
-                    return vector<int>{i, j};
-                }
+
+        unordered_map<int, int> num_map;
+        for(int i=0; i<nums.size(); i++) {
+            int complement = target - nums[i];
+            if(num_map.find(complement) != num_map.end()) {
+                return {num_map[complement], i};
             }
+            num_map[nums[i]] = i;
         }
-        return vector<int>{};
+        return {};
     }
 };
 
@@ -22,8 +40,9 @@ public:
 int main() {
 
     Solution s;
-    vector<int> nums = {2,7,11,15};
-    vector<int> ans = s.twoSum(nums, 9);
+    // vector<int> nums = {2,7,11,15};
+    vector<int> nums = {3,2,4};
+    vector<int> ans = s.twoSum(nums, 6);
     for(int i=0; i<ans.size(); i++) {
         cout<< ans[i]<< endl;
     }
