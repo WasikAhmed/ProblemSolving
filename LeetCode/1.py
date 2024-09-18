3a989fb9675c0a2b75397a467e98ec6e40d2925a
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-09-19 00:01:00 +0600

    Add python solution for LeetCode prob 1(Two Sum)

---
+++ LeetCode/1.py
@@ -0,0 +1,19 @@
+# Two Sum
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[num] = i
+        return []
+
+
+s = Solution()
+print(s.twoSum([2, 7, 11, 15], 9))
+print(s.twoSum([3, 2, 4], 6))
+print(s.twoSum([3, 3], 6))
